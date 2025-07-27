4476de0b5cf6729f598bdd18066bfa7d7f343335
rurikden <rurik.den@gmail.com>
2025-07-27 16:24:07 +0300

    Add title 'Змей Горыныч' and 'Работа' button

--- app/src/main/java/com/example/zmeygorynych/MainActivity.kt
+++ app/src/main/java/com/example/zmeygorynych/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.zmeygorynych
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Настройка обработчика нажатия кнопки
+        val btnWork = findViewById<Button>(R.id.btnWork)
+        btnWork.setOnClickListener {
+            Toast.makeText(this, "Кнопка 'Работа' нажата!", Toast.LENGTH_SHORT).show()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
