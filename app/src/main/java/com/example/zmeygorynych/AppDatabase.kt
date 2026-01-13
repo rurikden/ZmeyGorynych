3407831c474aafe610e1e9bd58955042842a43af
rurikden <rurik.den@gmail.com>
2026-01-13 09:19:18 +0300

    Fix Room database version mismatch - increased version to 3

--- app/src/main/java/com/example/zmeygorynych/AppDatabase.kt
+++ app/src/main/java/com/example/zmeygorynych/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
         entities = [Personnel::class, WorkType::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
